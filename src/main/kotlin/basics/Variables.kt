b2abcd2f86581f20971d61aa468a8d5b7ece3703
Bullets-jyr <jyr@bullets.co.kr>
2023-08-17 23:51:51 +0900

    4. Variables

---
+++ src/main/kotlin/basics/Variables.kt
@@ -0,0 +1,14 @@
+package basics
+
+fun main() {
+//    var first = 1
+//    first = 2
+//    val second = 2
+//    second = 3
+//    println(first + second)
+    var a = 10
+    a = 20
+    val b = a
+    println(a)
+    println(b)
+}
