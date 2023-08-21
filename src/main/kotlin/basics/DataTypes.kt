d906beef1ef54e275e90e688f9263cf5d29d9dd6
Bullets-jyr <jyr@bullets.co.kr>
2023-08-21 10:55:26 +0900

    5. Data Types

---
+++ src/main/kotlin/basics/DataTypes.kt
@@ -0,0 +1,21 @@
+package basics
+
+fun main() {
+    // byte, short, int, long
+    // float, double
+    val number: Int = 5
+    val doubleNumber = 4.99
+    val floatNumber = 4.99f
+    val letter = 'A'
+    val boolean = true
+//    var text = "Some random text"
+//    text = 1
+    var text = "5"
+    val numberFromString = Integer.parseInt(text)
+
+    val example = 1 + 1.5
+    val example2 = "Number" + 1.25
+//    val example2 = true + false
+//    val example2 = 5 + 'a'
+    println(example2)
+}
