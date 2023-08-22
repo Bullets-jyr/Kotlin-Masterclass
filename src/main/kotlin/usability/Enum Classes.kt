4106b3399f84f4099baf25daea5d543e0aa3b988
Bullets-jyr <jyr@bullets.co.kr>
2023-08-22 11:14:39 +0900

    27. Enum Classes

---
+++ src/main/kotlin/usability/Enum Classes.kt
@@ -0,0 +1,32 @@
+package usability
+
+fun main() {
+    val priority = Priority.LOW
+    println(priority.number)
+//    println(priority)
+//    println(priority.color)
+
+    val priority2 = Priority.valueOf(value = "LOW")
+    println(priority2)
+
+    for (pri in Priority.values()) {
+        println(pri.color)
+    }
+}
+
+//enum class Priority {
+//    LOW, MEDIUM, HIGH
+//}
+
+enum class Priority(val color: String) {
+    LOW(color = "Green"),
+    MEDIUM(color = "Orange"),
+    HIGH(color = "Red");
+
+    val number: Int
+        get() = when (this) {
+            LOW -> 1
+            MEDIUM -> 2
+            HIGH -> 3
+        }
+}
